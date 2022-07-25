838a84256e1d5fe299e4b3a002f4b5e15d76e284
XUXU <corpochta@gmail.com>
2022-07-25 17:12:33 +0300

    1.7. Конструктор [#173889 #323315]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -1,23 +1,32 @@
 package ru.job4j.oop;
 
-public class Error {
-    private boolean active;
-    private int status;
-    private String message;
+    public class Error {
+        private boolean active;
+        private int status;
+        private String message;
 
-    public Error(boolean active, int status, String message) {
+        public Error() {
+        }
+
+        public Error(boolean active, int status, String message) {
+            this.active = active;
+            this.status = status;
+            this.message = message;
+        }
+
+        public void printInfo() {
+            System.out.println(active);
+            System.out.println(status);
+            System.out.println(message);
+        }
+
+        public static void main(String[] args) {
+            Error warning = new Error();
+            warning.printInfo();
+            Error error = new Error(false, 123, "message");
+            error.printInfo();
+            Error notification = new Error(true, 404, "Page not found");
+            notification.printInfo();
+        }
     }
-
-    public static void main(String[] args) {
-        Error error = new Error(false, 123, "message");
-        error.printInfo();
-    }
-
-    public void printInfo() {
-        System.out.println(active);
-        System.out.println(status);
-        System.out.println(message);
-    }
-}
-
 
