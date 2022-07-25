9653c6a8b58d9506dd351bd6469bc9c10ed82742
XUXU <corpochta@gmail.com>
2022-07-25 17:17:40 +0300

    1.7. Конструктор [#173889 #323315]

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -1,8 +1,8 @@
-package ru.job4j.tracker.oop;
+package ru.job4j.oop;
 
 public class Ball {
-    public void tryRun(boolean condition){
-        if(condition){
+    public void tryRun(boolean condition) {
+        if (condition) {
             System.out.println("Колобок сьеден");
         } else {
             System.out.println("Колобок сбежал");
