807a03d62163b11b0c5411e71b246f2830aa3161
XUXU <corpochta@gmail.com>
2022-11-13 15:14:56 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,4 +61,16 @@ import java.util.Arrays;
          }
          return false;
      }
+
+     public boolean delete(int id) {
+         int index = indexOf(id);
+         if (index != -1) {
+             System.arraycopy(items, index + 1, items, index, size - index - 1);
+             items[size - 1] = null;
+             size--;
+             return true;
+         }
+         return false;
+     }
  }
+
