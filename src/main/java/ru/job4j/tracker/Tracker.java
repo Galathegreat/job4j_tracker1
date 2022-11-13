a870d258520ae7052ee41ae6e5af86384b5d4f89
XUXU <corpochta@gmail.com>
2022-11-13 14:18:14 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,42 +2,63 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 
  public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+     private final Item[] items = new Item[100];
+     private int ids = 1;
+     private int size = 0;
 
-    public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
-    }
+     public Item add(Item item) {
+         item.setId(ids++);
+         items[size++] = item;
+         return item;
+     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
-    }
+     public Item findById(int id) {
+         Item rsl = null;
+         for (int index = 0; index < size; index++) {
+             Item item = items[index];
+             if (item.getId() == id) {
+                 rsl = item;
+                 break;
+             }
+         }
+         return rsl;
+     }
 
-    public Item[] findALL() {
-        return Arrays.copyOf(items, size);
-    }
+     public Item[] findALL() {
+         return Arrays.copyOf(items, size);
+     }
 
-    public Item[] findByName(String key) {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (key.equals(items[i].getName())) {
-                rsl[count] = items[i];
-                count++;
-            }
-        }
-        return Arrays.copyOf(rsl, count);
-    }
-}
+     public Item[] findByName(String key) {
+         Item[] rsl = new Item[size];
+         int count = 0;
+         for (int i = 0; i < size; i++) {
+             Item item = items[i];
+             if (key.equals(items[i].getName())) {
+                 rsl[count] = items[i];
+                 count++;
+             }
+         }
+         return Arrays.copyOf(rsl, count);
+     }
+
+     private int indexOf(int id) {
+         int rsl = -1;
+         for (int index = 0; index < size; index++) {
+             if (items[index].getId() == id) {
+                 rsl = index;
+                 break;
+             }
+         }
+         return rsl;
+     }
+
+     public boolean replace(int id, Item item) {
+         int index = indexOf(id);
+         if (index != -1) {
+             item.setId(id);
+             items[index] = item;
+             return true;
+         }
+         return false;
+     }
+ }
