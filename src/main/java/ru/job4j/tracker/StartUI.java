633ba5988812a28ab1e8a2487844c6130467d8e0
XUXU <corpochta@gmail.com>
2022-11-25 12:26:13 +0300

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748 #362785]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,22 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ===");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+        System.out.println("Добавленная заявка: " + item);
+    }
         public void init(Input input, Tracker tracker) {
             boolean run = true;
             while (run) {
                 showMenu();
                 int select = input.askInt("Select: ");
                 if (select == 0) {
+                    StartUI.createItem(input, tracker);
+
                     System.out.println("=== Create a new Item ===");
                     String name = input.askStr("Enter name: ");
                     Item item = new Item(name);
