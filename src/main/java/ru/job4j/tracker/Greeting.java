2337883a570d42147f48e5f50d6f3264fe48142c
XUXU <corpochta@gmail.com>
2022-11-28 14:54:06 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780 #365268]

--- src/main/java/ru/job4j/tracker/Greeting.java
+++ src/main/java/ru/job4j/tracker/Greeting.java
@@ -9,26 +9,4 @@ public class Greeting {
         String name = input.nextLine();
         System.out.println(name + ", Nice to meet u!");
     }
-
-    public static class ShowAllItem implements UserAction {
-
-        @Override
-        public String name() {
-            return "Show all items";
-        }
-
-        @Override
-        public boolean execute(Input input, Tracker tracker) {
-            System.out.println("=== Show all items ===");
-            Item[] items = tracker.findAll();
-            if (items.length > 0) {
-                for (Item item : items) {
-                    System.out.println(item);
-                }
-            } else {
-                System.out.println("The repository does not contain requests.");
-            }
-            return true;
-        }
-    }
 }
