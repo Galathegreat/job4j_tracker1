92af96cb84e6403c4652135023765788698d42cb
XUXU <corpochta@gmail.com>
2023-01-09 15:21:10 +0300

    0.3. Тестирование исключений с junit

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,7 +1,7 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public void calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("N could not be less then 0");
         }
@@ -9,6 +9,7 @@ public class Fact {
         for (int i = 2; i <= n; i++) {
             rsl *= i;
         }
+        return rsl;
     }
 
     public static void main(String[] args) {
---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactTest {
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Fact.calc(-10);
+                });
+        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
+    }
+
+    @Test
+    public void when5fakt120() {
+        int num = 5;
+        int expected = 120;
+        int result = Fact.calc(num);
+        assertThat(result).isEqualTo(expected);
+    }
+}
